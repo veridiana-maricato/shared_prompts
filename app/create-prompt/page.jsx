2cabdfb77caeed8294f07ace5e46c72096085b09
veridianamaricato <maricato.veridiana@gmail.com>
2023-08-23 12:00:35 -0300

    form ready

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+"use client"
+import { useState } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+
+import Form from '@components/Form'
 
 const CreatePrompt = () => {
+    const router = useRouter()
+    const [submitting, setSubmitting] = useState(false)
+    const [post, setPost] = useState({
+        prompt: '',
+        tag: ''
+    })
+
+    const createPrompt = async (e) => {
+        e.preventDefault()
+        setSubmitting(true)
+
+        try {
+            const res = await fetch('/api/prompt/new',
+                {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        prompt: post.prompt,
+                        userId: session?.user.id,
+                        tag: post.tag
+                    })
+                })
+
+            if (res.ok) {
+                router.push('/')
+            }
+        } catch (e) {
+            console.log(e)
+        } finally {
+            setSubmitting(false)
+        }
+    }
+
     return (
-        <div>CreatePrompt</div>
+        <Form
+            type="Create"
+            post={post}
+            setPost={setPost}
+            submitting={submitting}
+            handleSubmit={createPrompt}
+        />
     )
 }
 
